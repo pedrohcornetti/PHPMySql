d4bdb05c9c0530fccbb21378e232a0d923739aeb
pedrohcornetti <pedrocornetti@outlook.com>
2024-05-11 10:24:43 -0300

    criando produtos

--- produtos/index.php
+++ produtos/index.php
@@ -5,7 +5,39 @@
 
     <h1>Gerenciamento de Produtos</h1>
 
-    <a href="" class="btn btn-primary">Adicionar Produto </a>
+    <a href="" class="btn btn-primary mt-3">Adicionar Produto </a>
+
+    <table class="mt-3 table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Persuasão</td>
+                <td>Livro de Jane Austen</td>
+                <td>R$ 10,00</td>
+                <td>Livros</td>
+            </tr>
+            <tr>
+                <td>Espagueti Liane</td>
+                <td>Macarrão de Semolina</td>
+                <td>R$ 3,00</td>
+                <td>Perecíveis</td>
+            </tr>
+            <tr>
+                <td>Óculos de Natação</td>
+                <td>Infantil</td>
+                <td>R$ 25,00</td>
+                <td>Material Esportivo</td>
+            </tr>
+        </tbody>
+
+    </table>
 
 <?php
     require_once("../rodape.html");
