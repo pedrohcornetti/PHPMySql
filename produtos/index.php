7fcaa42e8c92b503a9fc9f85dcecbfa8b6623b03
pedrohcornetti <pedrocornetti@outlook.com>
2024-05-11 10:35:56 -0300

    update index.php

--- produtos/index.php
+++ produtos/index.php
@@ -22,18 +22,27 @@
                 <td>Livro de Jane Austen</td>
                 <td>R$ 10,00</td>
                 <td>Livros</td>
+                <td><a href="alterar_produto.php" class="btn btn-warning">Alterar</a>
+                    <a href="excluir_produto.php" class="btn btn-danger">Excluir</a> 
+                </td>
             </tr>
             <tr>
                 <td>Espagueti Liane</td>
                 <td>Macarrão de Semolina</td>
                 <td>R$ 3,00</td>
                 <td>Perecíveis</td>
+                <td><a href="alterar_produto.php" class="btn btn-warning">Alterar</a>
+                    <a href="excluir_produto.php" class="btn btn-danger">Excluir</a> 
+                </td>
             </tr>
             <tr>
                 <td>Óculos de Natação</td>
                 <td>Infantil</td>
                 <td>R$ 25,00</td>
                 <td>Material Esportivo</td>
+                <td><a href="alterar_produto.php" class="btn btn-warning">Alterar</a>
+                    <a href="excluir_produto.php" class="btn btn-danger">Excluir</a> 
+                </td>
             </tr>
         </tbody>
 
