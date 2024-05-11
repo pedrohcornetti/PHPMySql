5d92bf482b1b8c8b443959bf84d9b6153450c284
pedrohcornetti <pedrocornetti@outlook.com>
2024-05-11 10:09:52 -0300

    teste

---
+++ produtos/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once("../cabecalho.html");
+?>
+
+    <h1>Gerenciamento de Produtos</h1>
+
+    <a href="" class="btn btn-primary">Adicionar Produto </a>
+
+<?php
+    require_once("../rodape.html");
